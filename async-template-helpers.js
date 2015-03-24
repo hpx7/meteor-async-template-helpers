183e7548ceff4f072b63105daf7e7cac9e79a8ed
Harsh Pandey <hpandey793@gmail.com>
2015-03-24 00:29:13 -0700

    upgrade to meteor 1.0.4

--- async-template-helpers.js
+++ async-template-helpers.js
@@ -1,17 +1,15 @@
 RegisterAsyncHelper = function (options, fn) {
-  var template = options.template, helperName = options.helperName, shared = options.shared;
+  var template = options.template, helperName = options.helperName;
 
   var value = new ReactiveVar();
 
-  var setupAutorun = function () {
-    Template.instance().autorun(function () {
+  template.onCreated(function () {
+    this.autorun(function () {
       fn(function (result) {
         value.set(result);
       });
     });
-  }
-
-  template.created = shared ? _.once(setupAutorun) : setupAutorun;
+  });
 
   var helper = {};
   helper[helperName] = function () {
--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'hpx7:async-template-helpers',
   summary: 'Use asynchronous functions inside your template helpers',
-  version: '0.3.4',
+  version: '0.4.0',
   git: 'https://github.com/hpx7/meteor-async-template-helpers.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.0.3.1');
+  api.versionsFrom('METEOR@1.0.4');
   api.use(['templating', 'underscore', 'reactive-var'], 'client');
   api.addFiles('async-template-helpers.js', 'client');
   api.export('RegisterAsyncHelper', 'client');
